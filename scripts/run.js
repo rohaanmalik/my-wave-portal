f72eb8c63656774ef6b143abf508eb5f86a8c931
rohaan malik <rohaanmalik7@gmail.com>
2021-08-14 10:14:25 -0400

    made my first smart contract

---
+++ scripts/run.js
@@ -0,0 +1,12 @@
+async function main() {
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy();
+    console.log("Contract deployed to:", waveContract.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
