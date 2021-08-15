500f7b1965d6b65267ebac7cde7c28a0c3c110c9
rohaan malik <rohaanmalik7@gmail.com>
2021-08-15 15:14:47 -0400

    added some waves functionality

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -3,7 +3,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal{
-constructor(){
-    console.log("I am a smart contract");
+    uint totalWaves;
+
+    constructor(){
+        console.log("I am a smart contract");
+    }
+
+    function waves() public {
+       totalWaves += 1;
+       console.log("%s is waved", msg.sender);
+    }
+
+    function getTotalWaves() view public returns (uint) {
+        console.log("We have %d total number of waves", totalWaves);
+        return totalWaves;
     }
 }
